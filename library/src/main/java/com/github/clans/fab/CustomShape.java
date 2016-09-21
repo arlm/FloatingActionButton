d37a4d657edd96d1f40a8fb33b104265d1e73a84
Pepa <pepa.hruska@gmail.com>
2016-09-21 19:19:36 +0200

    CR fix

--- library/src/main/java/com/github/clans/fab/CustomShape.java
+++ library/src/main/java/com/github/clans/fab/CustomShape.java
@@ -17,15 +17,16 @@ public class CustomShape extends RectShape {
     private float mRadius = 75f;
     private float mExtraTopShadow = -7f;
     private float mExtraLeftShadow = -5f;
-    private float mButtonPadding = 80f;
+    private float mButtonPadding;
 
     CustomShape(Context ctx) {
         super();
         mContext = ctx;
+        mButtonPadding = ctx.getResources().getDimensionPixelSize(R.dimen.custom_shape_buttonPadding);
     }
 
     @Override
     public void draw(Canvas canvas, Paint paint) { //
-            canvas.drawRoundRect(new RectF(Util.dpToPx(mContext, mExtraLeftShadow), Util.dpToPx(mContext, mExtraTopShadow), Util.getScreenWidth(mContext) - Util.dpToPx(mContext, mButtonPadding), Util.dpToPx(mContext, 50f)),mRadius,mRadius, paint);
+            canvas.drawRoundRect(new RectF(Util.dpToPx(mContext, mExtraLeftShadow), Util.dpToPx(mContext, mExtraTopShadow), Util.getScreenWidth(mContext) - mButtonPadding, Util.dpToPx(mContext, 50f)),mRadius,mRadius, paint);
     }
 }
